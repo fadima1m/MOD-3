52be27c617e483a117f6ba4cb416fff4c6d50e6b
fadima1m <129239149+fadima1m@users.noreply.github.com>
2023-06-19 02:34:13 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        //creating a new object of the the class Date
+        java.util.Date date = new java.util.Date();
+        System.out.println("Lab 15.1: "date);
+        System.out.println(System.getProperty("Programmer: "user.name));
+
+        //setting the variable
+        float x = 4600000 * 0.62
+        System.out.printf("East Coast Sales Prediction is x = %.2f\n");
     }
 }
---
+++ project06-01.js
@@ -0,0 +1,34 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 6
+      Project 06-01
+
+      Project to validate a form used for setting up a new account
+      Author: Fadima Macalou
+      Date:   06-12-2023
+
+      Filename: project06-01.js
+*/
+let submitButton = document.getElementById("submitButton")
+
+let pwd = document.getElementById("pwd")
+
+let pwd2 = document.getElementById("pwd2")
+
+submitButton.addEventListener('click', function() {
+
+      if (pwd.validity.patternMismatch) {
+
+            pwd.setCustomValidity("Your password must be at least 8 characters with at least one letter and one number");
+
+      } else if (pwd2.value != pwd.value) {
+
+            pwd2.setCustomValidity("Your passwords must match");
+
+      } else {
+
+            pwd2.setCustomValidity("")
+
+      }
+
+});
